15ea97603398349122fcac56bc9c50ade735d2c5
Diamond Zhou <diamondfsd@gmail.com>
2023-04-25 15:23:44 +0800

    feat

--- label_studio_hotkey.js
+++ label_studio_hotkey.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         label_studio_hotkey
 // @namespace    https://github.com/full-stack-study/tampermonkey-script
-// @version      1.7
+// @version      1.8
 // @description  给label_studio添加一些自定义的快捷键!
 // @author       DiamondFsd
 // @match        http://lablestudio.shanhs.com.cn/*

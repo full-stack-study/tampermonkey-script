e573777aca3eb04d77c3885fe6b69c5a30a2bf51
Diamond Zhou <diamondfsd@gmail.com>
2023-07-07 09:12:04 +0800

    feat: remove

--- label_studio_hotkey.js
+++ label_studio_hotkey.js
@@ -97,11 +97,7 @@ function __lb_add_js(url) {
         create_button('打开检测拍照', () => {
             const img_url = Array.from(document.querySelectorAll('.lsf-main-view .ant-typography')).map(a => a.innerText).filter(a => a.indexOf('http') > -1)[0]
             window.open(img_url)
-        })
-        create_button('清除标注', () => {
-            const task_id = get_task_id()
-            clear_annotation_and_prediect_and_to_next(task_id)
-        }, 'r')
+        })      
         create_button('移动到项目18', move_to_project_18, 't')
         
     }
